b556e6d7a10637127147a93931d65a25dd23fc26
Gareth Johns <info@garethjohnsdesign.com>
2017-04-18 22:11:16 +0100

    Initial Commit

--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -19,7 +19,7 @@ $('.index__carousel').flickity({
   contain: true,
   setGallerySize: false,
   wrapAround: true,
-  autoPlay: false,
+  autoPlay: true,
   prevNextButtons: false,
   pageDots: false,
   arrowShape: 'M100,46.875H11.9688l17.4688-17.4688L25,25L0,50l25,25l4.4062-4.4062L11.9688,53.125H100V46.875z'
